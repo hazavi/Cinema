c1fdcd142a91e741561184bfa0c81dedf34255f0
hazavi <kyyguevvara@gmail.com>
2024-12-12 20:38:26 +0100

    ok

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrl: './admin.component.css',
 })
 export class AdminComponent {
+  isDashboardContentVisible = true; // Dashboard content is visible by default
+  isUserContentVisible = false; 
 
+  toggleUserContent() {
+    if (this.isDashboardContentVisible) {
+      this.isDashboardContentVisible = false;
+      this.isUserContentVisible = true;
+    }
+  }
+
+  toggleDashboardContent() {
+    if (this.isUserContentVisible) {
+      this.isUserContentVisible = false;
+      this.isDashboardContentVisible = true;
+    }
+  }
 }
